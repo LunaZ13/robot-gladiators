d9c7082a00cd29f4e07ea5fdd5cad7206b6a6828
Inmar Luna <inmarluna@Inmars-MacBook-Pro.local>
2021-08-28 19:34:24 -0500

    console log

---
+++ assets/js/game.js
@@ -0,0 +1,35 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("test");
+console.log(10 + 10);
+console.log("our robots name is " +playerName);
+function fight() {
+    window.alert("The fight has begun");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fight();
